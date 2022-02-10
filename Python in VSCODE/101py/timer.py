b5e4f275d554b4b0a17951ca4098eab34d33b67d
hsoahdowhjd <85369936+hsoahdowhjd@users.noreply.github.com>
2022-02-09 20:27:31 -0500

    Update timer.py

--- Python in VSCODE/101py/timer.py
+++ Python in VSCODE/101py/timer.py
@@ -5,6 +5,7 @@ def count_down_timer(seconds):
         sec=int(seconds%60)
         timer=f'{minits}:{sec}'
         print(timer)
+        time.sleep(1) 
         seconds-=1
     print("time up")
 seconds=int(input("how many seconds: "))
